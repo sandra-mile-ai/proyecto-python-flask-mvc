4600620cf9c73b63c9c87bea048213502a09cac0
sandra <sc288488@gmail.com>
2025-04-20 11:36:36 -0500

    Actualiza tipos.py

--- src/models/tipos.py
+++ src/models/tipos.py
@@ -1,10 +1,15 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from src.models import session, Base
+from sqlalchemy import Column, Integer, String
+from src.models import Base, session
 
 class Tipos(Base):
-    __tablename__ ="tipos"
-    id = Column(Integer, primary_key=True)
-    tipo = Column(String(300), unique=True, nullable=False)
+    __tablename__ = "tipos"
 
-    def __init__(self, tipo):
-        self.tipo = tipo
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(300), unique=True, nullable=False)  
+
+def __init__(self, nombre):
+        self.tipos = nombre 
+
+def obtener_tipos():
+        tipos = session.query(Tipos).all()
+        return tipos 
